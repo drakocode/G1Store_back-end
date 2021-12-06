cc5ae9c4af56f1d98c82071f53f4531b29eeb27b
resyakKire <erikkayser843@gmail.com>
2021-12-06 15:37:32 -0300

    adicionando configurações pro banco de dados

--- g1store/src/main/java/com/Grupo1/G1Store/G1StoreApplication.java
+++ g1store/src/main/java/com/Grupo1/G1Store/G1StoreApplication.java
@@ -14,35 +14,10 @@ import java.util.List;
 public class G1StoreApplication {
 	public G1StoreApplication() {
 	}
-//	private static ProdutoService produtoService;
-//	private static CategoriaService categoriaService;
-//
-//	public G1StoreApplication(ProdutoService produtoService) {
-//		this.produtoService = produtoService;
-//	}
-//
-//	public G1StoreApplication(CategoriaService categoriaService) {
-//		this.categoriaService=categoriaService;
-//	}
-
-//	static List<Produto> produtos = new ArrayList<>();
-//	static List<Categoria> categorias = new ArrayList<>();
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(G1StoreApplication.class, args);
-		//
-//		Categoria nuclear=new Categoria("Nuclear");
-//		Categoria isotopo=new Categoria("Isotopo");
-//
-//
-//
-//		Produto p0=new Produto("uranio", 5555.0,"uranio enriquecido com alta volatilidade","img",nuclear);
-//
-//		Produto p1=new Produto("protio", 5555.0,"uranio enriquecido com alta volatilidade","img",isotopo);
-//		Produto p2=new Produto("tritio", 5555.0,"uranio enriquecido com alta volatilidade","img",isotopo);
-//		Produto p3=new Produto("deuterio", 5555.0,"uranio enriquecido com alta volatilidade","img",isotopo);
-//
 
 
 
@@ -50,21 +25,6 @@ public class G1StoreApplication {
 
 
 
-//		categorias.add(nuclear);
-//		categorias.add(isotopo);
-//
-//		produtos.add(p0);
-//		produtos.add(p1);
-//		produtos.add(p2);
-
-//		produtoService.buscarDadosDoProdutoPorId(1L);
-//		produtoService.cadastrarProduto(p0);
-//		produtoService.cadastrarProduto(p1);
-//		produtoService.cadastrarProduto(p2);
-//		produtoService.cadastrarProduto(p3);
-//
-//		categoriaService.cadastrarNovaCategoria(nuclear);
-//		categoriaService.cadastrarNovaCategoria(isotopo);
 
 	}
 }
